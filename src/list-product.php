339dc9249174289b0780bc570be9834974b74be5
Ana Gauna <amgauna@gmail.com>
2021-12-06 20:34:38 -0300

    Create list-product.php

---
+++ src/list-product.php
@@ -0,0 +1,21 @@
+<?php 
+// Importamos o autoload do composer
+require_once __DIR__.'/../config/bootstrap.php';
+
+// Como vamos trabalhar com envio de dados passados pelo terminal
+// apenas garanto que haja valor para que seja processado a ação.
+if(isset($entityManager)){
+    //Importamos o repository que nos auxiliará com a pesquisa.
+    $productRepository = $entityManager->getRepository('DiegoBrocanelli\Product');
+    
+    // Podemos acessar o método findAll() responsável por retornar todos os 
+    // registros cadastrados em nossa tabela products
+    $products = $productRepository->findAll();
+
+    // Realizamos uma iteração de dados
+    foreach ($products as $product) {
+        // Exibimos o resultado de cada registro encontrado
+        var_dump($product);
+    }
+}
+?>
