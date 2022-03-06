6d01dc8f3e73e868234771f30ee1090a806dfac3
Ana Gauna <amgauna@gmail.com>
2022-03-05 22:37:40 -0300

    Update list-product.php

--- src/app/models/list-product.php
+++ src/app/models/list-product.php
@@ -1,12 +1,12 @@
 <?php 
 // Importamos o autoload do composer
-require_once __DIR__.'/../config/bootstrap.php';
+require_once __DIR__.'../src/app/config/bootstrap.php';
 
 // Como vamos trabalhar com envio de dados passados pelo terminal
 // apenas garanto que haja valor para que seja processado a ação.
 if(isset($entityManager)){
     //Importamos o repository que nos auxiliará com a pesquisa.
-    $productRepository = $entityManager->getRepository('DiegoBrocanelli\Product');
+    $productRepository = $entityManager->getRepository('nome-email\Product');
     
     // Podemos acessar o método findAll() responsável por retornar todos os 
     // registros cadastrados em nossa tabela products
